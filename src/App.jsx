a4625444baec66c46fc36c640c2399ee6a2e43a1
abdul0325 <hrehman@gmail.com>
2025-08-16 10:46:17 +0500

    initial

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Header from "./header";
+
+function App() {
+  return <div>
+    <Header />
+  </div>;
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import Home from './components/home/home'
+import Navbar from './components/navbar/Navbar'
+
+function Header() {
+  return (
+    <div>
+        <Navbar />
+        <Home />
+    </div>
+  )
+}
+
+export default Header
